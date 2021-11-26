939aab3f6b3f4989787e1412eb3f7b040cf38902
MaximoDiNapoli <maximodinapolimail@gmail.com>
2021-11-26 17:03:44 -0300

    DTO de organización

---
+++ src/main/java/com/alkemy/ong/model/dto/DTOOrganization.java
@@ -0,0 +1,67 @@
+package com.alkemy.ong.model.dto;
+
+public class DTOOrganization {
+    private String name;
+    private String image;
+    private String address;
+    private int phone;
+    private String email;
+    private String welcomeText;
+    private String aboutUsText;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWelcomeText() {
+        return welcomeText;
+    }
+
+    public void setWelcomeText(String welcomeText) {
+        this.welcomeText = welcomeText;
+    }
+
+    public String getAboutUsText() {
+        return aboutUsText;
+    }
+
+    public void setAboutUsText(String aboutUsText) {
+        this.aboutUsText = aboutUsText;
+    }
+}
