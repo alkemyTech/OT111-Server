0de1f13e667cd5878eae370f115ba188ede54937
Tomas Padilla <padillatomasagustin@gmail.com>
2021-11-26 16:31:30 +0100

    OT111-15 - Se creo Modelo y Repositorio(Limpieza con Sonar)

--- src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
+++ src/main/java/com/alkemy/ong/repository/OrganizationRepository.java
@@ -2,7 +2,9 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.model.entity.OrganizationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrganizationRepository extends JpaRepository<OrganizationEntity, Integer> {
 
 }
