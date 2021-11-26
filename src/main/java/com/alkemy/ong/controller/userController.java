8f1752741d524d4c89538a1c36c3c4bbbcf02177
Cristian Silvestro <silvestro.cristian@gmail.com>
2021-11-26 20:07:26 -0300

    Continuacion de tarea: validacion de atributos dentro de userService (falta foto)

---
+++ src/main/java/com/alkemy/ong/controller/userController.java
@@ -0,0 +1,5 @@
+package com.alkemy.ong.controller;
+
+public class userController {
+
+}
---
+++ src/main/java/com/alkemy/ong/service/errorService.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.service;
+
+public class errorService extends Exception {
+
+    public errorService (String message){
+        super(message);
+    }
+
+}
