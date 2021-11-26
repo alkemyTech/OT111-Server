32eb548cd73feb8f46f04942d57d9fc9e84d8387
Tomas Padilla <padillatomasagustin@gmail.com>
2021-11-27 00:50:43 +0100

    OT111-15 - Script, Entity y Repositorio de Organization

--- src/main/java/com/alkemy/ong/model/entity/OrganizationEntity.java
+++ src/main/java/com/alkemy/ong/model/entity/OrganizationEntity.java
@@ -2,13 +2,11 @@ package com.alkemy.ong.model.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
+import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "organization")
@@ -20,46 +18,31 @@ public class OrganizationEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "id")
     private Integer id;
 
-    @Column (name = "name", nullable = false)
     private String name;
 
-    @Column (name = "image", nullable = false)
     private String image;
 
-    @Column (name = "address")
     private String address;
 
-    @Column (name = "phone")
     private Integer phone;
 
-    @Column (name = "email", nullable = false)
     private String email;
 
-    @Column (name = "welcomeText", nullable = false)
     private String welcomeText;
 
-    @Column (name = "aboutUsText", nullable = false)
     private String aboutUsText;
 
 //    Timestamps
-    @CreationTimestamp
-    @Column (name = "created_date", nullable = false)
-    private Timestamp createdDate;
+    private OffsetDateTime createdDate;
 
-    @Column (name = "created_by")
     private String createdBy;
 
-    @UpdateTimestamp
-    @Column (name = "modified_date")
-    private Timestamp modifiedDate;
+    private OffsetDateTime modifiedDate;
 
-    @Column (name = "modified_by")
     private String modifiedBy;
 
 //    Soft Delete
-    @Column(name = "deleted", nullable = false)
     private boolean deleted = Boolean.FALSE;
 }
