a8d16a4bd1def950080dce6d046de1317bb6ab6a
denisdimarco <denisdimarcoleda@gmail.com>
2021-11-26 17:17:57 -0300

    OT111-22 - Added sql script to create members table

---
+++ scripts/v0.1__members.sql
@@ -0,0 +1,18 @@
+create table if not EXISTS members (
+--  Fields
+--TODO Poner tamano de VARCHAR?
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    facebook_url VARCHAR NULL,
+    instagram_url VARCHAR NULL,
+    linkedin_url VARCHAR NULL,
+    image VARCHAR NOT NULL,
+    description VARCHAR NULL,
+--  Timestamps
+    created_date timestamptz(0) NOT NULL DEFAULT NOW(),
+    created_by varchar(100) NULL,
+    modified_date timestamptz(0) NULL,
+    modified_by varchar(100) NULL,
+--  SoftDelete
+    deleted bool NOT NULL DEFAULT FALSE
+);
