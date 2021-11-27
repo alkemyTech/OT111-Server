9b4ec139432b4dbd39fba08740eaf8c7af4d3091
HugoLucero <hugolucerofotografo@gmail.com>
2021-11-26 21:23:12 -0300

    OT111 - 16 - Se creó modelo y repositorio de Roles.

---
+++ src/main/java/com/alkemy/ong/model/entity/Role.java
@@ -0,0 +1,38 @@
+package com.alkemy.ong.model.entity;
+
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name= "roles")
+@SQLDelete(sql = "UPDATE role SET deleted = true WHERE id=?")
+@Where(clause = "deleted=false")
+public class Role {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRole;
+
+    private String name;
+
+    private String description;
+
+    private OffsetDateTime createdDate;
+
+    private String createdBy;
+
+    private OffsetDateTime modifiedDate;
+
+    private String modifiedBy;
+
+    private boolean deleted = Boolean.FALSE;
+}
