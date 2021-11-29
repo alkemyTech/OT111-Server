095d5de7a547a67c6fdd0e2c18de8489136e2663
Tomas Padilla <padillatomasagustin@gmail.com>
2021-11-29 22:34:12 +0100

    OT111-15 - Fixed PR

--- scripts/V0.1__organization.sql
+++ scripts/V0.1__organization.sql
@@ -1,1 +1,19 @@
-git
+CREATE TABLE  organization (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    image TEXT NOT NULL,
+    address TEXT NULL,
+    phone INT NULL,
+    email TEXT NOT NULL,
+    welcomeText TEXT NOT NULL,
+    aboutUsText TEXT NULL,
+--    Timestamps
+    created_date timestamptz(0) NOT NULL DEFAULT NOW(),
+    created_by TEXT(100) NULL,
+    modified_date timestamptz(0) NULL,
+    modified_by TEXT(100) NULL,
+--    Soft Delete
+    deleted bool NOT NULL DEFAULT FALSE,
+
+    CONSTRAINT organization_pk PRIMARY KEY (id)
+);
