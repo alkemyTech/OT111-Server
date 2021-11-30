11632ef7dae8bd2e475521c879af44d2fa117f56
Tomas Padilla <padillatomasagustin@gmail.com>
2021-11-30 15:31:32 +0100

    OT111-15 - Fixed PR: Text paa welcome y aboutUs

--- scripts/V0.1__organization.sql
+++ scripts/V0.1__organization.sql
@@ -1,17 +1,17 @@
 CREATE TABLE  organization (
     id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
-    name TEXT NOT NULL,
-    image TEXT NOT NULL,
-    address TEXT NULL,
+    name VARCHAR(255) NOT NULL,
+    image VARCHAR(500) NOT NULL,
+    address VARCHAR(255) NULL,
     phone INT NULL,
-    email TEXT NOT NULL,
+    email VARCHAR(255) NOT NULL,
     welcomeText TEXT NOT NULL,
     aboutUsText TEXT NULL,
 --    Timestamps
     created_date timestamptz(0) NOT NULL DEFAULT NOW(),
-    created_by TEXT(100) NULL,
+    created_by VARCHAR(100) NULL,
     modified_date timestamptz(0) NULL,
-    modified_by TEXT(100) NULL,
+    modified_by VARCHAR(100) NULL,
 --    Soft Delete
     deleted bool NOT NULL DEFAULT FALSE,
 
