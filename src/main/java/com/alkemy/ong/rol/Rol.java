64df6cd2d55128042d1ae48e4004ac53245b8e32
JuanOrtiz <ortizjuani2001@gmail.com>
2021-11-30 19:09:18 -0300

    Aded Rol

---
+++ src/main/java/com/alkemy/ong/rol/Rol.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.rol;
+
+public enum Rol {
+
+    ROLE_ADMIN,
+    ROLE_USER;
+
+}
---
+++ src/main/java/com/alkemy/ong/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.alkemy.ong.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsCustomServiceImpl userDetailsCustomServiceImpl;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsCustomServiceImpl(userDetailsCustomServiceImpl);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/admin").hasRole(ROLE_ADMIN)
+                .and()
+                .formLogin();
+    }
+
+
+}
