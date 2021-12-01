1debacd42849184ea33c0ee25253fa230b54580d
danit3x <dvargas087@gmail.com>
2021-12-01 19:01:46 -0300

    OT111-21, PR changes fixed.

--- src/main/java/com/alkemy/ong/repository/ActivitiesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alkemy.ong.repository;
-
-import com.alkemy.ong.model.entity.ActivitiesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActivitiesRepository extends JpaRepository <ActivitiesEntity, Integer>{
-}
