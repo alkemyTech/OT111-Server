55bcec4f15818e9a11b23aa1e5c254aab74f95c1
Tomas Padilla <padillatomasagustin@gmail.com>
2021-12-01 01:24:17 +0100

    OT111-25 - Edited getAuthorities()

--- src/main/java/com/alkemy/ong/auth/model/SecurityUser.java
+++ src/main/java/com/alkemy/ong/auth/model/SecurityUser.java
@@ -2,10 +2,13 @@ package com.alkemy.ong.auth.model;
 
 import com.alkemy.ong.model.entity.UserEntity;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 public class SecurityUser implements UserDetails {
 
@@ -17,7 +20,9 @@ public class SecurityUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        //TODO: GetRole - GrantedAuthorities
+           //TODO: GetRole - GrantedAuthorities
+        List<GrantedAuthority> authorities = new ArrayList<>();
+//        authorities.add(new SimpleGrantedAuthority(user.getRole()));
         return Collections.emptyList();
     }
 
