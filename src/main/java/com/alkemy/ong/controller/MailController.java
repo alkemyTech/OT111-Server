93bc4a8832791fbd3d5ba8471b665666771bfe24
Alejandro Jofre <Alejandrojofre2014@gmail.com>
2021-12-02 19:44:47 -0300

    se agrego MailControler y MailService

---
+++ src/main/java/com/alkemy/ong/controller/MailController.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping(value = "/api")
+public class MailController {
+
+    @Autowired
+
+
+}
---
+++ src/main/java/com/alkemy/ong/service/MailService.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.service;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+@Service
+public class MailService {
+    private static final Logger logger = LoggerFactory.getLogger(MailService.class);
+
+    public String
+
+}
