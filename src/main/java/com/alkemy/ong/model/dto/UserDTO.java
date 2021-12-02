a9a86c62e46b1e5ecd0dfb2eff5314b513db6906
Cristian Silvestro <silvestro.cristian@gmail.com>
2021-12-01 21:07:21 -0300

    Corregido el bud de Dto a DTO

--- src/main/java/com/alkemy/ong/model/dto/UserDTO.java
+++ src/main/java/com/alkemy/ong/model/dto/UserDTO.java
@@ -1,15 +1,32 @@
 package com.alkemy.ong.model.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class UserDTO {
 
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @Email
     private String email;
+
+    @NotEmpty
     private String password;
+
+    private String photo;
+
+    private String role;
+
     private String jwt;
 }
