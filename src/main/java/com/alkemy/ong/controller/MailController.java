1cf5e35b5e41795f6e90f23663f4055e25b4f862
Alejandro Jofre <Alejandrojofre2014@gmail.com>
2021-12-03 13:36:16 -0300

    modifico MailController, Modifico Mail Sercvice para envio de mails, Modifico un error de tipeo en el yml

--- src/main/java/com/alkemy/ong/controller/MailController.java
+++ src/main/java/com/alkemy/ong/controller/MailController.java
@@ -1,13 +1,24 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
 @RestController
 @RequestMapping(value = "/api")
 public class MailController {
 
     @Autowired
+    MailService mailService;
+
+    @PostMapping("/send-text")
+    public String send()throws IOException{
+        return mailService.SEND();
+    }
 
 
 }
