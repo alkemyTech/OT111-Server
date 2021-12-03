e4d11576ef266066acfc3ef7e1dfd2c7b2bcf707
Cristian Silvestro <silvestro.cristian@gmail.com>
2021-12-02 21:03:18 -0300

    Agregado @Entity en UserEntity

--- src/main/java/com/alkemy/ong/model/entity/UserEntity.java
+++ src/main/java/com/alkemy/ong/model/entity/UserEntity.java
@@ -15,6 +15,7 @@ import java.time.OffsetDateTime;
 @Table(name = "users")
 @SQLDelete(sql = "UPDATE users SET deleted = true WHERE id=?")
 @Where(clause = "deleted = false")
+@Entity
 public class UserEntity {
 
     private static final long serialVersionUID = 1L;
