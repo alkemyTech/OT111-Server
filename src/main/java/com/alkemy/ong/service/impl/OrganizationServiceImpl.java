5581e54bfb0fd2e614729a4fecafff19655ada63
MaximoDiNapoli <maximodinapolimail@gmail.com>
2021-12-04 19:49:25 -0300

    Update OrganizationServiceImpl.java

--- src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
@@ -4,8 +4,11 @@ import com.alkemy.ong.model.entity.OrganizationEntity;
 import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.service.OrganizationService;
 
+import javax.transaction.Transactional;
+
 public class OrganizationServiceImpl implements OrganizationService {
     @Override
+    @Transactional
     public OrganizationEntity readOrganization(Long id) {
         return OrganizationRepository.getById(id);
     }
