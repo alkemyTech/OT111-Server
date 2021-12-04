aa208e805ba04a1d08e29b02e1116d39e75bd26f
Tomas Padilla <padillatomasagustin@gmail.com>
2021-12-05 00:47:53 +0100

    Segurity -  Added Script User-Role , ManyToMany

---
+++ scripts/TEST_UserRole.sql
@@ -0,0 +1,9 @@
+create table user_role(
+	usersid int8 ,
+	rolesid int8 ,
+	CONSTRAINT usersrolesid PRIMARY KEY (usersid, rolesid),
+  	CONSTRAINT usersid_roles_id_fk FOREIGN KEY (usersid)
+      REFERENCES users (id),
+  	CONSTRAINT rolesid_usersid_fk FOREIGN KEY (rolesid)
+      REFERENCES roles (id)
+)
