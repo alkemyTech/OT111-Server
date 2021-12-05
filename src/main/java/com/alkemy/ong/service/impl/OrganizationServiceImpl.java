b330dc44ed44c95098b75d8482ae99c267419af8
MaximoDiNapoli <maximodinapolimail@gmail.com>
2021-12-05 13:38:26 -0300

    OT111-23 Delete @Transactional

--- src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
@@ -3,13 +3,15 @@ package com.alkemy.ong.service.impl;
 import com.alkemy.ong.model.entity.OrganizationEntity;
 import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.service.OrganizationService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.transaction.Transactional;
 
 public class OrganizationServiceImpl implements OrganizationService {
+    @Autowired
+    private OrganizationRepository organizationRepository;
     @Override
-    @Transactional
     public OrganizationEntity readOrganization(Long id) {
-        return OrganizationRepository.getById(id);
+        return organizationRepository.getById(id);
     }
 }
