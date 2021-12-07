2596aa5244ef0a6aec70f56aa929b45e8e6ff0fc
danit3x <dvargas087@gmail.com>
2021-12-07 17:14:10 -0300

    Fix getcategories

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -1,24 +1,28 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.model.dto.CategoryDTO;
 import com.alkemy.ong.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/categories")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping(value = "/categories", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getCategories(){
+    @GetMapping
+    public ResponseEntity <List<CategoryDTO>> getCategories(){
 
-        return ResponseEntity.ok(categoryService.getCategories());
+        return new ResponseEntity<>(categoryService.getCategories(), HttpStatus.OK);
 
     }
 }
