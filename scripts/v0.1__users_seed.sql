7010ad9ef8ae39f93d0f63e8e6935ed7907ba2cf
denisdimarco <denisdimarcoleda@gmail.com>
2021-12-07 13:23:05 -0300

    OT111-72 - v0.1__users_seed.sql

---
+++ scripts/v0.1__users_seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO roles
+(id, "name")
+VALUES (1, 'ROLE_USER'),
+       (2, 'ROLE_ADMIN');
+
+INSERT INTO users
+(id, "name")
+VALUES (1, 'ROLE_USER'),
+       (2, 'ROLE_ADMIN');
+
+INSERT INTO users
+    (id, first_name, last_name, email, password, photo)
+    VALUES  (1, 'Admin1', 'Admin1', 'Admin1@email.com', '$2a$10$CE8BbdcbEKwo6suHhigUm.0H233zB9QxDH7EWvo3ujligpY6cHP52', 'MyPhoto.jpg')
+
+
+
+
+
+
