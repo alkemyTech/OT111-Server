8c483d0ccb4ed6a8331805181c3148441b162613
Tomas Padilla <padillatomasagustin@gmail.com>
2021-12-08 16:38:56 +0100

    OT111-30 - Transactional FIX

--- src/main/java/com/alkemy/ong/auth/service/UserDetailsCustomService.java
+++ src/main/java/com/alkemy/ong/auth/service/UserDetailsCustomService.java
@@ -18,7 +18,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
