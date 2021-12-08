94821ec616fff441d958fecf970ae850347b3240
Diego Martinez Giardini <diego.martinez.giardini@gmail.com>
2021-12-08 16:08:54 -0300

    AWS configuration

---
+++ src/main/java/com/alkemy/ong/controller/AWSBucketController.java
@@ -0,0 +1,26 @@
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.service.AWSService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/storage")
+public class AWSBucketController {
+
+    @Autowired
+    private AWSService awsService;
+
+    @PostMapping(value = "/upload")
+    public ResponseEntity<String> uploadFile(@RequestPart(value = "file")MultipartFile file) {
+        awsService.uploadFile(file);
+        String response = "File " + file.getOriginalFilename() + " uploaded successfully.";
+        return new ResponseEntity<String>(response, HttpStatus.OK);
+    }
+}
