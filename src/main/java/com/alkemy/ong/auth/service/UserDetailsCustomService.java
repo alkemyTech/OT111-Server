741e42557a6b883744a750755aa25369c5e4dc13
Tomas Padilla <padillatomasagustin@gmail.com>
2021-12-08 16:49:30 +0100

    OT111-99 -  Hechos 2 Handlers, buscando ejemplos

--- src/main/java/com/alkemy/ong/auth/service/UserDetailsCustomService.java
+++ src/main/java/com/alkemy/ong/auth/service/UserDetailsCustomService.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
-import java.util.FormatterClosedException;
 import java.util.stream.Collectors;
 
 @Service
