360a856c440097349a4c73fe6352653372342202
danit3x <64555508+danit3x@users.noreply.github.com>
2021-12-09 19:38:35 -0300

    OT111-40v2 Endpoint Listado Categories (#37)

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.model.request.CategoryRequestDTO;
+import com.alkemy.ong.model.response.CategoryDTO;
 import com.alkemy.ong.model.response.CategoryResponseDTO;
 import com.alkemy.ong.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -34,6 +37,13 @@ public class CategoryController {
     public ResponseEntity<CategoryResponseDTO> createNewCategory(@RequestBody CategoryRequestDTO request){
         CategoryResponseDTO response = categoryService.saveCategory(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity <List<CategoryDTO>> getCategories(){
+
+        return new ResponseEntity<>(categoryService.getCategories(), HttpStatus.OK);
+
     }
 
 }
