4f2f53c690f821e9ce30c068c27bfc2b04574c17
denisdimarco <denisdimarcoleda@gmail.com>
2021-12-10 03:20:46 -0300

    OT111-84 - created OpenApiConfig & added swagger to UserController
     created OpenApiConfig for bearerAuth in Security of swagger
    Added Swagger anotation on UserControllers

--- src/main/java/com/alkemy/ong/controller/UserController.java
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -1,13 +1,21 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.model.entity.UserEntity;
 import com.alkemy.ong.model.response.user.UserDTO;
 import com.alkemy.ong.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
@@ -19,11 +27,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Obtener todos los usuarios.", security = @SecurityRequirement(name = "bearerAuth"),description = "Obtener lista completa de usuarios, solo accesible por administrador")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Se obtuvieron todos los usuarios en el sistema",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class))}),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Acceso denegado",
+                    content = @Content)})
     @GetMapping
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
     }
 }
 
-//TODO Pull Request para agregar Path /users en SecurityConfiguration para que lo pueda solo trabajar un admin
+
 
