ced2689f3c0f0e284ee41184157dc7a4549482da
denisdimarco <denisdimarcoleda@gmail.com>
2021-12-10 03:24:20 -0300

    OT111-84 - deleted unused import on UserController

--- src/main/java/com/alkemy/ong/controller/UserController.java
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
