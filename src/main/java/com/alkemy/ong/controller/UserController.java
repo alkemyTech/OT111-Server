1de77afc1edec143b0a7f5809d8b9346ba8819fb
Maximo Di napoli <63366993+MaximoDiNapoli@users.noreply.github.com>
2021-12-13 22:54:23 -0300

    Feature/ot111 32 (#51)

--- src/main/java/com/alkemy/ong/controller/UserController.java
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -12,6 +12,8 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +42,15 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        boolean isRemoved = userService.deleteUser(id);
+        if (!isRemoved) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
 
--- src/main/java/com/alkemy/ong/service/UserService.java
+++ src/main/java/com/alkemy/ong/service/UserService.java
@@ -6,4 +6,7 @@ import java.util.List;
 
 public interface UserService {
     List<UserDTO> findAllUsers();
+
+
+    boolean deleteUser(Long id);
 }
