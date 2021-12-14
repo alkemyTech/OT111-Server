b240f561e0157e2e59ba4e4a77f60f4f09c36d3a
MaximoDiNapoli <maximodinapolimail@gmail.com>
2021-12-14 10:16:02 -0300

    Update ActivityRequestDTO.java

--- src/main/java/com/alkemy/ong/model/request/ActivityRequestDTO.java
+++ src/main/java/com/alkemy/ong/model/request/ActivityRequestDTO.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.model.request;
 
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +9,10 @@ import javax.validation.constraints.NotEmpty;
 @Getter
 @Setter
 public class ActivityRequestDTO {
-    @NotEmpty(message = "El nombre es obligatorio")
+    @NotEmpty(message = "El nombre es obligatorio y no puede ser null")
+    @NotNull
     private String name;
-    @NotEmpty(message = "El contenido es obligatorio")
+    @NotEmpty(message = "El contenido es obligatorio y no puede ser null")
+    @NotNull
     private String content;
 }
