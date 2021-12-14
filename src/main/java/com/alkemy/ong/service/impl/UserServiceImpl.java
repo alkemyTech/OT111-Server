d2893e209e1aed6affa68e4d4f26e7d6ad0149eb
Tomas Padilla <padillatomasagustin@gmail.com>
2021-12-14 08:29:48 +0100

    OT111-53 - Contacts Model, Entity, Repo. deleteUser() fixed return

--- src/main/java/com/alkemy/ong/service/impl/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/UserServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -29,8 +28,9 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public boolean deleteUser(Long id) {
-
         var foundUser = userRepository.findById(id).orElseThrow();
         userRepository.delete(foundUser);
+        return true;
     }
+
 }
