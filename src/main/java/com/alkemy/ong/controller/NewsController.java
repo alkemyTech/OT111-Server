2e1ef3a49b604e021a980def541dab19492e64cd
denisdimarco <denisdimarcoleda@gmail.com>
2021-12-17 11:29:47 -0300

    OT111-81 - Created GET /users endpoint, getNewsPage method

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -1,17 +1,19 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.model.response.news.NewsDTO;
+import com.alkemy.ong.model.response.pagination.CustomPage;
 import com.alkemy.ong.service.NewsService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("news")
@@ -28,8 +30,10 @@ public class NewsController {
 
     @GetMapping
     @Operation(summary = "Lista todas las novedades")
-    public ResponseEntity<List<NewsDTO>> getNews() {
-        var response = newsService.getNews();
+    public ResponseEntity<CustomPage<NewsDTO>> getNewsPage(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
+                                                           @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+        Pageable pageRequest = PageRequest.of(page, size);
+        var response = newsService.getNewsPageable(pageRequest);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
