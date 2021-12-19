500f1cdd7dd8e4c70a44af57bc90cd327cc3169e
denisdimarco <denisdimarcoleda@gmail.com>
2021-12-19 00:28:00 -0300

    OT111-46v2 - Updated NewsController
    Modified createNews endpoint, changes to the others endpoints

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -1,17 +1,19 @@
 package com.alkemy.ong.controller;
 
-import com.alkemy.ong.model.response.news.NewsDTO;
+import com.alkemy.ong.model.request.NewsRequestDTO;
+import com.alkemy.ong.model.response.news.NewsResponseDTO;
 import com.alkemy.ong.service.NewsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,15 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("news")
+@RequiredArgsConstructor
 public class NewsController {
 
-    @Autowired
-    private NewsService newsService;
+    private final NewsService newsService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<NewsDTO> findById(@PathVariable Long id) {
-        NewsDTO newsDTO = newsService.findById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(newsDTO);
+    public ResponseEntity<NewsResponseDTO> findById(@PathVariable Long id) {
+        NewsResponseDTO newsResponseDTO = newsService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(newsResponseDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<NewsResponseDTO> createNews(@RequestBody NewsRequestDTO newsRequestDTO) {
+        NewsResponseDTO response = newsService.saveNews(newsRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PutMapping("/{id}")
@@ -37,11 +45,11 @@ public class NewsController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Novedad actualizada",
                     content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = NewsDTO.class))}),
+                            schema = @Schema(implementation = NewsResponseDTO.class))}),
             @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
     })
-    public ResponseEntity<Void> updateNews(@RequestBody NewsDTO newsDTO, @PathVariable Long id) {
-        newsService.updateNews(newsDTO, id);
+    public ResponseEntity<Void> updateNews(@RequestBody NewsResponseDTO newsResponseDTO, @PathVariable Long id) {
+        newsService.updateNews(newsResponseDTO, id);
         return ResponseEntity.ok().build();
     }
 }
