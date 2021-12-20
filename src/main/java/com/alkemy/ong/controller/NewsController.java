1b23158ecc42eb24b8565cc000250fd60037008b
Tomas Padilla <padillatomasagustin@gmail.com>
2021-12-20 19:57:07 +0100

    OT111-97 - Fixed Merge

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -14,9 +14,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<<HEAD
-import org.springframework.web.bind.annotation.*;
-=======
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>>develop
 
 @RestController
 @RequestMapping("news")
