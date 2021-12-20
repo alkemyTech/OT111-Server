23127637b6b57e4ae9b48d57be69e10e70b2a08f
Tomas Padilla <padillatomasagustin@gmail.com>
2021-12-20 17:30:49 +0100

    ot111slides - falta nombre y extension

--- src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/SlideServiceImpl.java
@@ -4,15 +4,18 @@ import com.alkemy.ong.model.entity.OrganizationEntity;
 import com.alkemy.ong.model.entity.SlideEntity;
 import com.alkemy.ong.model.mapper.SlideMapper;
 import com.alkemy.ong.model.request.SlideRequestDTO;
+import com.alkemy.ong.model.response.AWSResponseDTO;
 import com.alkemy.ong.model.response.SlideResponseDTO;
 import com.alkemy.ong.repository.OrganizationRepository;
 import com.alkemy.ong.repository.SlideRepository;
 import com.alkemy.ong.service.AWSService;
 import com.alkemy.ong.service.SlideService;
+import com.alkemy.ong.utils.BASE64DecodedMultipartFile;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Service;
 
-import java.util.Base64;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor //TODO: Se esta usando dos formas de injeccion de dependencia porfa normalizar auna
@@ -21,16 +24,25 @@ public class SlideServiceImpl implements SlideService {
     private final SlideRepository slideRepository;
     private final OrganizationRepository organizationRepository;
     private final AWSService awsService;
+    private final BASE64DecodedMultipartFile base64DecodedMultipartFile;
 
     @Override
     public SlideResponseDTO saveSlide(SlideRequestDTO request) {
         Long organizationID = request.getOrganizationId();
         OrganizationEntity orgEnt = organizationRepository.getById(organizationID);
-        byte[] misBytes = Base64.getDecoder().decode(request.getImagenCodificada());
 
-        String imagenUrl = awsService.uploadFile();
+        BASE64DecodedMultipartFile cosa = new BASE64DecodedMultipartFile(Base64.decodeBase64(request.getImagenCodificada()));
+        System.out.println("===============");
+        System.out.println(request.getImagenCodificada());
+        System.out.println(cosa.getOriginalFilename());
+        System.out.println("===============");
 
-        SlideEntity newSlide = slideMapper.slideDTO2Entity(request, orgEnt, imagenUrl);
+        AWSResponseDTO imagenUrl = awsService.uploadFile(cosa);
+        System.out.println("=========");
+        System.out.println(imagenUrl.getImageUrl());
+        System.out.println("=========");
+        SlideEntity newSlide = slideMapper.slideDTO2Entity(request, orgEnt, imagenUrl.getImageUrl());
+
         SlideEntity savedSlide = slideRepository.save(newSlide);
         return slideMapper.slideEntity2DTO(savedSlide);
     }
---
+++ src/main/java/com/alkemy/ong/utils/BASE64DecodedMultipartFile.java
@@ -0,0 +1,57 @@
+package com.alkemy.ong.utils;
+
+import com.amazonaws.util.Base64;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+@Service
+public class BASE64DecodedMultipartFile implements MultipartFile {
+    private final byte[] imgContent;
+
+    public BASE64DecodedMultipartFile(byte[] imgContent) {
+        this.imgContent = imgContent;
+    }
+
+    @Override
+    public String getName() {
+        return "null";
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return "Hola.png";
+    }
+
+    @Override
+    public String getContentType() {
+        // TODO - implementation depends on your requirements
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return imgContent == null || imgContent.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return imgContent.length;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        return imgContent;
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return new ByteArrayInputStream(imgContent);
+    }
+
+    @Override
+    public void transferTo(File dest) throws IOException, IllegalStateException {
+        new FileOutputStream(dest).write(imgContent);
+    }
+}
