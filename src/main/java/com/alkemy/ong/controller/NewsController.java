4bae1c5222b65731f3b11a8ada4088e77f7453fb
denisdimarco <denisdimarcoleda@gmail.com>
2021-12-21 13:09:03 -0300

    OT111-86v2 - Swagger Simplification, Added @ResponseStatus to ApiExcectionHandler methods
    Se agrego el handleMethodArgumentNotValid en ApiExceptionHandler

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -1,21 +1,17 @@
 package com.alkemy.ong.controller;
 
-import com.alkemy.ong.exception.ApiException;
 import com.alkemy.ong.model.request.NewsRequestDTO;
 import com.alkemy.ong.model.response.news.NewsResponseDTO;
 import com.alkemy.ong.model.response.pagination.CustomPage;
 import com.alkemy.ong.service.NewsService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
+@Validated
 @RequestMapping("news")
 @Tag(name = "Novedades")
 @RequiredArgsConstructor
@@ -39,26 +36,6 @@ public class NewsController {
     @Operation(summary = "Obtener novedad por ID", description = "Obtiene los detalles de" +
             " la novedad por el ID pasado como parámetro por url, " +
             "y si no existe se lanza un error con código de estado 404")
-    @ApiResponses(
-            {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "OK",
-                            content = {
-                                    @Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = NewsResponseDTO.class))
-                            }
-                    ),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Novedad no encontrada",
-                            content = {
-                                    @Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = ApiException.class))
-                            }
-                    ),
-            }
-    )
     @GetMapping("/{id}")
     public ResponseEntity<NewsResponseDTO> findById(@PathVariable Long id) {
         NewsResponseDTO newsResponseDTO = newsService.findNewsById(id);
@@ -70,34 +47,6 @@ public class NewsController {
                     "Retornara los datos cargados de novedad y de su categoría asociada " +
                     "En caso que se omita algún dato requerido se devolverá mensaje de error, asi mismo " +
                     "se comprobará si la categoría asociada existen en el sistema, si no se lanza un error con código de estado 404.")
-    @ApiResponses(
-            {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "OK",
-                            content = {
-                                    @Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = NewsResponseDTO.class))
-                            }
-                    ),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "Constraint Violation: Datos no validos",
-                            content = {
-                                    @Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = ApiException.class))
-                            }
-                    ),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Categoría no encontrada",
-                            content = {
-                                    @Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = ApiException.class))
-                            }
-                    ),
-            }
-    )
     @PostMapping
     public ResponseEntity<NewsResponseDTO> createNews(@Valid @RequestBody NewsRequestDTO newsRequestDTO) {
         NewsResponseDTO response = newsService.saveNews(newsRequestDTO);
@@ -109,10 +58,6 @@ public class NewsController {
             description = "Obtiene todas las novedades, con un tamaño por defecto de pagina de 10 elementos, " +
                     "ademas retorna el numero de pagina, cantidad de elementos en la pagina, cantidad de elementos total y " +
                     "cantidad total de paginas. Opcionalmente puede ingresar como parámetros el numero de pagina o cantidad de elementos por pagina.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomPage.class))})})
     @GetMapping
     public ResponseEntity<CustomPage<NewsResponseDTO>> getNewsPage(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
                                                                    @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
@@ -127,35 +72,6 @@ public class NewsController {
                     "Retornara los datos cargados de novedad y ademas de la categoría asociada " +
                     "En caso que se omita algún dato requerido se devolverá mensaje de error, asi mismo " +
                     "se comprobará si la novedad y su categoría asociada existen en el sistema si no se lanza un error con código de estado 404 ")
-
-    @ApiResponses(
-            {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "OK",
-                            content = {
-                                    @Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = NewsResponseDTO.class))
-                            }
-                    ),
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "Constraint Violation: Datos no validos",
-                            content = {
-                                    @Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = ApiException.class))
-                            }
-                    ),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Novedad o Categoría no encontrada",
-                            content = {
-                                    @Content(mediaType = "application/json",
-                                            schema = @Schema(implementation = ApiException.class))
-                            }
-                    ),
-            }
-    )
     @PutMapping("/{id}")
     public ResponseEntity<NewsResponseDTO> updateNews(@Valid @RequestBody NewsRequestDTO newsRequestDTO, @PathVariable Long id) {
         NewsResponseDTO response = newsService.updateNews(newsRequestDTO, id);
@@ -166,20 +82,6 @@ public class NewsController {
     @Operation(summary = "Eliminar una Novedad",
             description = "Elimina la novedad existente dado el ID pasado como parámetro por url, " +
                     "y si la categoría a eliminar no existe se lanza un error con código de estado 404")
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "204",
-                    description = "Novedad Eliminada",
-                    content = @Content),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "Novedad o Categoría no encontrada",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = ApiException.class))
-                    }
-            )
-    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteNewsById(@PathVariable Long id) {
         newsService.deleteNews(id);
--- src/main/java/com/alkemy/ong/exception/ApiExceptionHandler.java
+++ src/main/java/com/alkemy/ong/exception/ApiExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.alkemy.ong.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolation;
@@ -17,6 +22,7 @@ import java.util.NoSuchElementException;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {NoSuchElementException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ApiException> noSuchElementExceptionHandler(NoSuchElementException noSuchElementException) {
         var apiException = new ApiException(
                 noSuchElementException.getMessage(),
@@ -26,6 +32,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(value = {AuthenticationException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     public ResponseEntity<ApiException> authenticationExceptionHandler(AuthenticationException authenticationException) {
         var apiException = new ApiException(
                 authenticationException.getMessage(),
@@ -35,6 +42,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(value = {IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ApiException> illegalArgumentExceptionHandler(IllegalArgumentException illegalArgumentException) {
         var apiException = new ApiException(
                 illegalArgumentException.getMessage(),
@@ -62,6 +70,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ApiConstraintViolationException> constraintViolationException(ConstraintViolationException constraintViolationException) {
         List<String> details = new ArrayList<>();
         for (ConstraintViolation<?> violation : constraintViolationException.getConstraintViolations()) {
@@ -74,5 +83,24 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 .build();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiConstraintViolationException);
     }
+
+    @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        List<FieldError> errorFields = ex.getBindingResult().getFieldErrors();
+
+        for (FieldError e : errorFields) {
+            details.add(e.getField() + " : " + e.getDefaultMessage());
+        }
+        var apiConstraintViolationException = ApiConstraintViolationException.builder()
+                .message("Constraint Violations")
+                .httpStatus(HttpStatus.BAD_REQUEST.value())
+                .errors(details)
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiConstraintViolationException);
+    }
+
 
 }
