dee9323bae71200a4efe60b0e9e39946875d6966
danit3x <dvargas087@gmail.com>
2021-12-27 22:20:27 -0300

    se limpian anotaciones

--- src/main/java/com/alkemy/ong/service/impl/ActivityServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/ActivityServiceImpl.java
@@ -5,14 +5,10 @@ import com.alkemy.ong.model.mapper.ActivityMapper;
 import com.alkemy.ong.model.request.ActivityRequest;
 import com.alkemy.ong.model.request.ActivityUpdateRequestDTO;
 import com.alkemy.ong.model.response.ActivityResponseDTO;
-import com.alkemy.ong.model.response.ActivityUpdateResponseDTO;
 import com.alkemy.ong.repository.ActivityRepository;
 import com.alkemy.ong.service.ActivityService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.OffsetDateTime;
 
 @RequiredArgsConstructor
 @Service
@@ -24,27 +20,22 @@ public class ActivityServiceImpl implements ActivityService {
 
 
     @Override
-    public ActivityResponseDTO createActivity(ActivityRequest request, String userName){
-        ActivityEntity newActivity = ActivityMapper.activityDTO2Entity(request, userName);
+    public ActivityResponseDTO createActivity(ActivityRequest request){
+        ActivityEntity newActivity = ActivityMapper.activityDTO2Entity(request);
         ActivityEntity savedActivity = activityRepository.save(newActivity);
         return ActivityMapper.activityEntity2DTO(savedActivity);
     }
 
     @Override
-    public ActivityUpdateResponseDTO updateActivity(ActivityUpdateRequestDTO request, Long id, String userName) {
-
-        OffsetDateTime updateTime = OffsetDateTime.now();
+    public ActivityResponseDTO updateActivity(ActivityUpdateRequestDTO request, Long id) {
 
         ActivityEntity foundActivity = activityRepository.findById(id).orElseThrow();
         foundActivity.setName(request.getName());
         foundActivity.setContent(request.getContent());
         foundActivity.setImage(request.getImage());
-        foundActivity.setModifiedDate(updateTime);
-        foundActivity.setModifiedBy(userName);
-
 
         ActivityEntity saved = activityRepository.save(foundActivity);
 
-        return activityMapper.activityUpdateEntity2DTO(saved);
+        return activityMapper.activityEntity2DTO(saved);
     }
 }
